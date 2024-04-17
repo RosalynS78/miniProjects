49c239e5b57e857f871dab68280a7e77ad09a90e
Rosalyn Scheffel <rrcervania78@gmail.com>
2024-04-16 23:48:09 -0500

    light dark

--- lightDark/index.js
+++ lightDark/index.js
@@ -1,7 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var modeSwitch = document.querySelector('.mode-switch');
+// document.addEventListener('DOMContentLoaded', function () {
+//     var modeSwitch = document.querySelector('.mode-switch');
   
-    modeSwitch.addEventListener('click', function () {                     document.documentElement.classList.toggle('dark');
-      modeSwitch.classList.toggle('active');
-    });
-  });
+//     modeSwitch.addEventListener('click', function () {                     document.documentElement.classList.toggle('dark');
+//       modeSwitch.classList.toggle('active');
+//     });
+//   });
+
+
+// function myFunction() {
+//   var element = document.body;
+//   element.classList.toggle("dark-mode");
+
+// }
+
+
+// On toggler clicked
+
+var container = document.getElementsByClassName('container')[0];
+document.getElementById('toggler').addEventListener('change', (event) => {
+  console.log(toggler)
+  event.target.checked ? container.removeAttribute('data-theme') : container.setAttribute('data-theme', 'dark');
+});
